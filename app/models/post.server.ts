9455389f2c042dee7926a58eaee28864b1f02b2c
joannaotmianowska <otmianowska.joanna@gmail.com>
2023-06-28 12:53:13 +0200

    fix ts errors

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -9,7 +9,8 @@ export async function getPost(slug: string) {
     return prisma.post.findUnique({ where: { slug } });
 }
 
-export async function createPost(post) {
+export async function createPost(
     post: Pick<Post, "slug" | "title" | "markdown">
+  ) {
     return prisma.post.create({ data: post });
-}
+  }
