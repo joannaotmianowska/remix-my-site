1241d40c4f32b41a59d673693178a0c546be73a2
joannaotmianowska <otmianowska.joanna@gmail.com>
2023-08-07 21:42:07 +0200

    main photo & text mobile view

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -2,52 +2,23 @@ export default function Main() {
   return (
     <>
       <main className="mx-auto w-screen bg-my-beige font-extralight">
-        <div className="bg-inherit mx-4 flex items-center justify-center gap-20">
-          <div className="h-[55rem]">
+        <div className="mx-4 flex flex-col items-center justify-center bg-inherit lg:mx-4 lg:flex-row lg:gap-20">
+          <div className="lg:h-[55rem]">
             <img
-              className="object-contain max-h-full h-[55rem]"
+              className="max-h-full object-contain lg:h-[55rem]"
               alt="main my portrait"
               src="https://github.com/joannaotmianowska/my-site/assets/20688756/e058e077-57e5-4d20-bac8-04c5a0ac39a4"
             />
           </div>
-          <h5 className="text-3xl max-w-lg leading-10">
-          Cześć! Jestem Joanna. Szukam pretekstów do rozmyślań i
-          rozmów. Programuję, piszę, robię kreatywne rzeczy, staram się łapać balans i mam sto nowych pomysłów na minutę. Piszę newslettery, w których zachęcam do wspólnego rozmyślania - możesz przeczytać je w archiwum albo zapisać się poniżej i dostawać je regularnie. 
+          <h5 className="text-xl leading-10 lg:max-w-lg lg:text-3xl">
+            Cześć! Jestem Joanna. Szukam pretekstów do rozmyślań i rozmów.
+            Programuję, piszę, robię kreatywne rzeczy, staram się łapać balans i
+            mam sto nowych pomysłów na minutę. Piszę newslettery, w których
+            zachęcam do wspólnego rozmyślania - możesz przeczytać je w archiwum
+            albo zapisać się poniżej i dostawać je regularnie.
           </h5>
         </div>
       </main>
-      {/* <main className="mx-auto w-screen bg-my-beige p-20">
-        <div className="flex-column max-w-8xl container mx-auto flex gap-20 bg-inherit sm:px-6 lg:px-8">
-          <div className="bg-inherit">
-            <img
-              className="h-full w-full object-cover"
-              alt="main my portrait"
-              src="https://github.com/joannaotmianowska/my-site/assets/20688756/e058e077-57e5-4d20-bac8-04c5a0ac39a4"
-            />
-          </div>
-          <div className="text-left font-bold leading-loose tracking-tight ">
-            <h2 className="py-20 text-black sm:text-4xl lg:text-5xl">
-              Kod. Nauka. Planowanie. Notatki. Macierzyństwo. Dobrostan. Balans.
-              Odpoczynek.
-            </h2>
-          </div>
-        </div>
-      </main>
-      <div className="sm:text-1xl mx-auto w-screen bg-my-violet p-20 text-left leading-loose text-white lg:text-2xl">
-        <p className="container">
-          Nazywam się Joanna Otmianowska. Szukam pretekstów do rozmyślań i
-          rozmów. Programuję, piszę, ćwiczę jogę, staram się łapać balans i mam
-          sto nowych pomysłów na minutę. Zachęcam kobiety do kodowania w FB
-          grupie "Programuj, dziewczyno!", a na moim koncie na IG pokazuję moje
-          poszukiwania dobrostanu. Teraz jestem na urlopie macierzyńskim, ale
-          poza życiem mamy pracuję jako programistka. W nowej odsłonie mojego
-          newslettera zapraszam Cię do wspólnego szukania pretekstów do
-          rozmyślań. Oprócz tematów związanych z IT, pojawią się też takie
-          związane z innymi aspektami życia. Masz ochotę razem porozkminiać?
-          Zapisz się i widzimy się w Twojej skrzynce mailowej co dwa tygodnie w
-          sobotni poranek.
-        </p>
-      </div> */}
     </>
   );
 }
